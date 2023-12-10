878741a934b719c0bdbd40da9f76d0dbd50bc426
Stephan F <Stephan.feige@yahoo.com>
2023-12-10 11:59:43 +0100

    Improved shared modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -13,29 +13,20 @@ import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { NgApexchartsModule } from 'ng-apexcharts';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NavbarCollapsedComponent } from './navbar-collapsed/navbar-collapsed.component';
-import { RouterModule } from '@angular/router';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { DashboardViewBarModule } from './dashboard-view-bar/dashboard-view-bar.module';
+import { NavbarModule } from './navbar/navbar.module';
+import { NavbarCollapsedModule } from './navbar-collapsed/navbar-collapsed.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule,
-    
-    MatIconModule,
-    MatListModule,
-    MatMenuModule,
-    MatTooltipModule
   ],
   declarations: [
-    NavbarComponent,
-    NavbarCollapsedComponent,
   ],
   exports: [
-    NavbarComponent,
-    NavbarCollapsedComponent,
+    NavbarModule,
+    NavbarCollapsedModule,
     DashboardViewBarModule,
 
     NgApexchartsModule,
