801f8cd87e40f43797ba07c38b0d459f34a240a9
Stephan F <Stephan.feige@yahoo.com>
2024-03-10 11:41:42 +0100

    Implement new components

--- src/app/modules/dashboard/device-setting/device-setting-details/device-setting-details.component.ts
+++ src/app/modules/dashboard/device-setting/device-setting-details/device-setting-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DeviceStatus } from '@models';
+import { DeviceService } from '@services/device-service';
 
 @Component({
   selector: 'app-device-setting-details',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device-setting-details.component.scss']
 })
 export class DeviceSettingDetailsComponent implements OnInit {
-
-  constructor() { }
+  deviceStatus?: DeviceStatus;
+  
+  constructor(private deviceService: DeviceService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.loadDeviceStatus(id);
+    });
   }
 
+  private loadDeviceStatus(id: string) {
+    this.deviceService.getDeviceStatus(id).subscribe(status => {
+      console.log(status);
+      this.deviceStatus = status;
+    });
+  }
 }
